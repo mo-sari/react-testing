33d671826f7c77f34d29dc9e5b06ecc78b447b67
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-26 18:21:43 +0330

    better way of querying the element one by one

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -19,7 +19,12 @@ test("tries to mimic intering name and email and submitting the form", () => {
   // try to render the component
   render(<UserForm addUser={mock} />);
   // find the two elements
-  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  const nameInput = screen.getByRole("textbox", {
+    name: /name/i,
+  });
+  const emailInput = screen.getByRole("textbox", {
+    name: /email/i,
+  });
   // simulate typing in a name
   user.click(nameInput);
   user.keyboard("mohsen");
