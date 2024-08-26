d59b6c6c171fa561ff3dd939fb551fd693cf6394
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-26 17:57:58 +0330

    testing the functionality of UserForm component

--- src/UserForm.test.js
+++ src/UserForm.test.js
@@ -13,3 +13,29 @@ test("it shows two inputs and a button", () => {
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
 });
+
+test("tries to mimic intering name and email and submitting the form", () => {
+  // Not the best implementation
+  // try to render the component
+  const argList = [];
+  const addArg = (...args) => {
+    argList.push(args);
+  };
+  render(<UserForm addUser={addArg} />);
+  // find the two elements
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+  // simulate typing in a name
+  user.click(nameInput);
+  user.keyboard("mohsen");
+  // simulate typing in an email
+  user.click(emailInput);
+  user.keyboard("mohsen@gmail.com");
+  // find the button
+  const button = screen.getByRole("button");
+  // simulate clicking the button
+  user.click(button);
+  // assertion to make sure 'onUserAdd' gets called with email/name
+  expect(argList).toHaveLength(1);
+  console.log(argList);
+  expect(argList[0][0]).toEqual({ name: "mohsen", email: "mohsen@gmail.com" });
+});
