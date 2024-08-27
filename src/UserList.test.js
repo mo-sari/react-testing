c37c2bc88e934796bbb51f4c2a23d6611c8ff866
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-27 18:08:14 +0330

    new test for userlist component

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -3,7 +3,6 @@ import user from "@testing-library/user-event";
 import UserList from "./UserList";
 
 test("render one row per user", () => {
-  // render the component
   const users = [
     {
       name: "ali",
@@ -15,17 +14,26 @@ test("render one row per user", () => {
     },
   ];
   const { container } = render(<UserList users={users} />);
-  //   container is a div that is covering all the html
-  // created by our component and it's created by default
-  // we can check that in logTestingPlaygroundURL();
-
-  //   find all the rows in the table
-
-  //   in previous solution we had to change our source-code
-  // solely for the purpose of testing, if that is not what you want:
   const rows = container.querySelectorAll("tbody tr");
-  //   tbody tr => is a regular css selector, you might want to search for that.
-
-  // assertion: correct number of rows in the table
   expect(rows).toHaveLength(2);
+});
+test("render the name and email of each user", () => {
+  const users = [
+    {
+      name: "ali",
+      email: "ali@gmail.com",
+    },
+    {
+      name: "hasan",
+      email: "hasan@gmail.com",
+    },
+  ];
+  render(<UserList users={users} />);
+  for (let user of users) {
+    const name = screen.getByRole("cell", { name: user.name });
+    const email = screen.getByRole("cell", { name: user.email });
+
+    expect(name).toBeInTheDocument();
+    expect(email).toBeInTheDocument();
+  }
 });
