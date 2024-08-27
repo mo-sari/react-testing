e4ee3de6c8d247f404a456baa8e75a7040c63243
mo-sari <mohsen.ansari.bim@gmail.com>
2024-08-27 18:13:37 +0330

    trying to refactor repeated code

--- src/UserList.test.js
+++ src/UserList.test.js
@@ -2,22 +2,7 @@ import { render, screen, within } from "@testing-library/react";
 import user from "@testing-library/user-event";
 import UserList from "./UserList";
 
-test("render one row per user", () => {
-  const users = [
-    {
-      name: "ali",
-      email: "ali@gmail.com",
-    },
-    {
-      name: "hasan",
-      email: "hasan@gmail.com",
-    },
-  ];
-  const { container } = render(<UserList users={users} />);
-  const rows = container.querySelectorAll("tbody tr");
-  expect(rows).toHaveLength(2);
-});
-test("render the name and email of each user", () => {
+const renderComponent = () => {
   const users = [
     {
       name: "ali",
@@ -29,6 +14,18 @@ test("render the name and email of each user", () => {
     },
   ];
   render(<UserList users={users} />);
+  return users;
+};
+
+test("render one row per user", () => {
+  const users = renderComponent();
+  const { container } = render(<UserList users={users} />);
+  const rows = container.querySelectorAll("tbody tr");
+  expect(rows).toHaveLength(2);
+});
+
+test("render the name and email of each user", () => {
+  const users = renderComponent();
   for (let user of users) {
     const name = screen.getByRole("cell", { name: user.name });
     const email = screen.getByRole("cell", { name: user.email });
